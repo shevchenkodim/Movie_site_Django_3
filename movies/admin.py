56653e951c40344382bfc96759d7691092883b2a
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-03-31 23:51:51 +0300

    Creation and output of films on the site
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Actor, Genre, Movie, MovieShot, RatingStars, Rating, Reviews
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShot)
+admin.site.register(RatingStars)
+admin.site.register(Rating)
+admin.site.register(Reviews)
