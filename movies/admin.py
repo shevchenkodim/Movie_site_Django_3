1e890e128695c022ce9c4b0eb7c2e818845b90f3
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-04-02 21:23:02 +0300

    Using the ckeditor editor in the django admin panel and youtube plugin
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Category, Actor, Genre, Movie, MovieShot, RatingStars, Rating, Reviews
 from django.utils.safestring import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class MovieAdminForm(forms.ModelForm):
+    description = forms.CharField(label='Описание', widget=CKEditorUploadingWidget ())
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 @admin.register(Category)
@@ -38,6 +47,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ('draft',)
+    form = MovieAdminForm
     readonly_fields = ('get_image',)
     fieldsets = (
             (None, {
