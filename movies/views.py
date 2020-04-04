d5079a0571eb83dab39394eff6f5e70ea09740c5
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-04-04 18:08:00 +0300

    template tags inclusion and simple
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- movies/views.py
+++ movies/views.py
@@ -9,13 +9,20 @@ class MovieView(ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    #template_name = "movies/movies.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        return context
 
 
 class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        return context
 
 
 class AddReview(View):
